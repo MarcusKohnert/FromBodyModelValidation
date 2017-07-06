155fc59ab18eb6f9ebd7eeaa745cc8ecfd0bc1cd
Marcus Kohnert <marcus.dev@live.com>
2017-07-06 21:31:11 +0200

    FromBody model is not validated

---
+++ WebApplication3/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpPut("{id:int}")]
+        public IActionResult Put(
+            int id,
+            [FromBody]SomeClass value)
+        {
+            if (this.TryValidateModel(value) == false)
+                throw new Exception();
+
+            return this.BadRequest(this.ModelState);
+        }
+    }
+
+    public class SomeClass
+    {
+        [StringLength(2)]
+        [Required(AllowEmptyStrings = false)]
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string Name { get; set; }
+    }
+}
---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvcCore()
+                .AddJsonFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
